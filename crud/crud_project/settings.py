ab4190888f5e0b6fc6ad990c95ab32454ee2f0f7
Vlad <35526774+vlad10100@users.noreply.github.com>
2021-11-06 23:16:35 +0800

    Update settings.py

--- crud/crud_project/settings.py
+++ crud/crud_project/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crud_app', 
-
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 
 MIDDLEWARE = [
